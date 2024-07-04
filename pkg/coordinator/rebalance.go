a2cec3fabd52c366037e982285b520e59c1342cf
alexli <know19923817@163.com>
2024-07-04 15:33:01 +0800

    update log

--- pkg/coordinator/rebalance.go
+++ pkg/coordinator/rebalance.go
@@ -356,15 +356,15 @@ func (c *Coordinator) assignNoScrapingTargets(
 	}
 
 	for hash, tar := range active {
-		c.log.Debugf("active target info: hash=%, job=%s, url=%s", hash, tar.Job, tar.PromTarget.URL().String())
+		c.log.Debugf("active target info: hash=%d, job=%s, url=%s", hash, tar.Job, tar.PromTarget.URL().String())
 		if scraping[hash] {
-			c.log.Debugf("target %s already scraping", hash)
+			c.log.Debugf("target %d already scraping", hash)
 			continue
 		}
 
 		status := globalScrapeStatus[hash]
 		if status == nil || status.Health != scrape.HealthGood {
-			c.log.Debugf("target %s not healthy", hash)
+			c.log.Debugf("target %d not healthy", hash)
 			continue
 		}
 
@@ -378,7 +378,7 @@ func (c *Coordinator) assignNoScrapingTargets(
 			sd.runtime.HeadSeries += status.Series
 			sd.scraping[hash] = status
 		} else {
-			c.log.Debugf("no shard free for %s, need space %d", hash, status.Series)
+			c.log.Debugf("no shard free for %d, need space %d", hash, status.Series)
 			needSpace += status.Series
 		}
 	}
